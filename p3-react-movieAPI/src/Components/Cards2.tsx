fa40ecbcb0601a1f5f9ed7b75ad7dd1dce1ec1bc
Faruk Miftari <farukmiftari994@gmail.com>
2024-01-25 15:56:05 +0100

    Fixed Firestore

--- p3-react-movieAPI/src/Components/Cards2.tsx
+++ p3-react-movieAPI/src/Components/Cards2.tsx
@@ -1,61 +1,61 @@
-// import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import { useState } from "react";
-import Model from "../modals/DetailsModal";
-import { FaStar } from "react-icons/fa";
+// // import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
+// import ListGroup from "react-bootstrap/ListGroup";
+// import { useState } from "react";
+// import Model from "../modals/DetailsModal";
+// import { FaStar } from "react-icons/fa";
 
-interface BasicExampleProps {
-  character: any;
-}
+// interface BasicExampleProps {
+//   character: any;
+// }
 
-const BasicExample: React.FC<BasicExampleProps> = ({ character }) => {
-  const [show, setShow] = useState(false);
+// const BasicExample: React.FC<BasicExampleProps> = ({ character }) => {
+//   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
 
-  return (
-    <div
-      className="p-0"
-      style={{
-        width: "260px",
-        height: "430px",
-        margin: "30px",
-        border: "white 2px solid",
-        borderRadius: "20px",
-      }}
-      onClick={handleShow}
-    >
-      <Card.Img
-        src={`https://image.tmdb.org/t/p/w500${character.poster_path}`}
-        style={{
-          width: "256px",
-          height: "350px",
-          borderTopRightRadius: "20px",
-          borderTopLeftRadius: "20px",
-        }}
-      />
-      <ListGroup variant="flush">
-        <div>
-          <ListGroup.Item className="d-flex justify-content-center align-items-center">
-            <b>{character.vote_average}</b>
-            <FaStar />
-          </ListGroup.Item>
-        </div>
-        <ListGroup.Item
-          className="d-flex justify-content-center"
-          style={{
-            borderBottomRightRadius: "20px",
-            borderBottomLeftRadius: "20px",
-          }}
-        >
-          <b>{character.name}</b>
-        </ListGroup.Item>
-      </ListGroup>
-      <Model show={show} handleClose={handleClose} character={character} />
-    </div>
-  );
-};
+//   return (
+//     <div
+//       className="p-0"
+//       style={{
+//         width: "260px",
+//         height: "430px",
+//         margin: "30px",
+//         border: "white 2px solid",
+//         borderRadius: "20px",
+//       }}
+//       onClick={handleShow}
+//     >
+//       <Card.Img
+//         src={`https://image.tmdb.org/t/p/w500${character.poster_path}`}
+//         style={{
+//           width: "256px",
+//           height: "350px",
+//           borderTopRightRadius: "20px",
+//           borderTopLeftRadius: "20px",
+//         }}
+//       />
+//       <ListGroup variant="flush">
+//         <div>
+//           <ListGroup.Item className="d-flex justify-content-center align-items-center">
+//             <b>{character.vote_average}</b>
+//             <FaStar />
+//           </ListGroup.Item>
+//         </div>
+//         <ListGroup.Item
+//           className="d-flex justify-content-center"
+//           style={{
+//             borderBottomRightRadius: "20px",
+//             borderBottomLeftRadius: "20px",
+//           }}
+//         >
+//           <b>{character.name}</b>
+//         </ListGroup.Item>
+//       </ListGroup>
+//       <Model show={show} handleClose={handleClose} character={character} />
+//     </div>
+//   );
+// };
 
-export default BasicExample;
+// export default BasicExample;
--- src/Components/ProtectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const ProtectedRoute = ({ children }: PropsWithChildren) => {
-  return <div>ProtectedRoute</div>;
-};
-
-export default ProtectedRoute;
